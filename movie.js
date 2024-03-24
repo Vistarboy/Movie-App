391b54cd1532bea419f167b1311153be6439d9ec
Vistarboy <151842693+Vistarboy@users.noreply.github.com>
2024-03-24 23:35:23 +0300

    Add files via upload
    This is a movie review app, which it fetches data form Open Movie Data Base (OMDB). Not for downloading, or watching them, but trailers and more info are provided when a movie is clicked.

---
+++ movie.js
@@ -0,0 +1,67 @@
+const apiLink = `http://www.omdbapi.com/?=&apikey=306dc500`;
+
+function searchMovie(url) {
+  fetch(url).then(response => response.json()).then(data => {
+    if (data.Response === 'True') {
+
+      let results = data.Search
+
+      const movieCollection = document.querySelector('.movie-collection')
+      movieCollection.innerHTML = '';
+
+      results.forEach(movie => {
+
+        const title = movie.Title
+        const year = movie.Year
+        const type = movie.Type
+        const movieID = movie.imdbID
+        const imageUrl = movie.Poster
+
+        const movieContainer = document.createElement('div')
+        movieContainer.setAttribute('class', 'movie-container')
+        movieContainer.innerHTML =
+          `<center>
+                    <a href="https://www.imdb.com/title/${movieID}" target="_blank">
+                      <img src="${imageUrl}" class="movie-image" alt="cover Image of ${movie.Title}">
+                    </a>
+                    <p id="movie-name">${title}<br>${year}</p>
+                  </center>
+                `
+
+        movieCollection.appendChild(movieContainer)
+      })
+
+    } else {
+      alert('Sorry, ' + data.Error)
+    }
+  })
+    .catch(error => {
+      console.error('Error in fetching data:', error)
+    })
+}
+
+
+const form = document.querySelector('.form')
+const input = document.getElementById('search-bar')
+const SearchButton = document.getElementById('search-icon')
+
+let searchApi = '', movieName = '';
+
+searchApi = `http://www.omdbapi.com/?s=avatar&apikey=306dc500`;
+
+searchMovie(searchApi);
+
+function getSearch() {
+  movieName = input.value
+  searchApi = `http://www.omdbapi.com/?s=${movieName}&apikey=306dc500`;
+
+  searchMovie(searchApi);
+
+  input.value = '';
+}
+form.addEventListener('submit', () => {
+  getSearch();
+})
+SearchButton.onclick = () => {
+  getSearch();
+}
